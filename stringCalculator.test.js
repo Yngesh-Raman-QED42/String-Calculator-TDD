46e5b763ca81b68ac9fe027274da2c787cdad9a7
Yngesh-Raman-QED42 <yngesh.raman@qed42.com>
2025-01-21 14:24:24 +0530

    Write test to add two numbers separated by comma

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -9,3 +9,8 @@ test("should return 0 for an empty string", () => {
 test("should return the number for a single number", () => {
 	expect(add("1")).toBe(1);
 });
+
+// Test 3
+test("should return the sum of two numbers separated by a comma", () => {
+	expect(add("1,2")).toBe(3);
+});
