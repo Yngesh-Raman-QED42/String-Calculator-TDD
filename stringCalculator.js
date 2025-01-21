330de988c8ef31f3535d8ed954eb2836fadf851f
Yngesh-Raman-QED42 <yngesh.raman@qed42.com>
2025-01-21 14:29:44 +0530

    update the 'add' function to clear test 4 as well

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,11 @@
 function add(numbers) {
 	if (numbers === "") return 0;
 
-	return numbers.split(",").reduce((sum, num) => sum + parseInt(num, 10), 0); // this will also handle the case of single number, so we can make it a default for now
+	const delimiter = /,|\n/;
+
+	return numbers
+		.split(delimiter)
+		.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
 module.exports = { add };
