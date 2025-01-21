5b966fc303265e4d4cbfa2982e004b182b1a60ef
Yngesh-Raman-QED42 <yngesh.raman@qed42.com>
2025-01-21 14:32:21 +0530

    update the 'add' function to pass test 5

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,15 @@
 function add(numbers) {
 	if (numbers === "") return 0;
 
-	const delimiter = /,|\n/;
+	let delimiter = /,|\n/;
+
+	if (numbers.startsWith("//")) {
+		delimiter = numbers[2]; // Single character delimiter
+		numbers = numbers.slice(4);
+		return numbers
+			.split(delimiter)
+			.reduce((sum, num) => sum + parseInt(num, 10), 0);
+	}
 
 	return numbers
 		.split(delimiter)
