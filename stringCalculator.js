fcd26f2997719507fad4860019766483026e8c45
Yngesh-Raman-QED42 <yngesh.raman@qed42.com>
2025-01-21 14:26:15 +0530

    update the 'add' function to pass the third test case as well

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,7 @@
 function add(numbers) {
 	if (numbers === "") return 0;
-	return parseInt(numbers, 10); // returning the number if its a single number
+
+	return numbers.split(",").reduce((sum, num) => sum + parseInt(num, 10), 0); // this will also handle the case of single number, so we can make it a default for now
 }
 
 module.exports = { add };
