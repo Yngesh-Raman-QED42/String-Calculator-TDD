a73d87d903e33303e31b6e15a716b5bc5b169155
Yngesh-Raman-QED42 <yngesh.raman@qed42.com>
2025-01-21 14:21:25 +0530

    write test for single number case

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,5 +1,11 @@
 const { add } = require("./stringCalculator.js");
 
+// Test 1
 test("should return 0 for an empty string", () => {
 	expect(add("")).toBe(0);
+});
+
+// Test 2
+test("should return the number for a single number", () => {
+	expect(add("1")).toBe(1);
 });
